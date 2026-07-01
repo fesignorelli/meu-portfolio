3deb5b18db7d1f4df24439634f9031e67409c7fa
fesignorelli <signorelli980@gmail.com>
2026-07-01 00:15:19 -0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,7 @@ export default defineNuxtConfig({
         },
       ],
       link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         {
